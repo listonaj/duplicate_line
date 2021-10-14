e01dba38e29cada4760cf38f6389d5afe3d148f6
listonaj <jm.romain@outlook.com>
2021-10-14 12:59:35 -0700

    wrote instructions to run dup1.go

--- dup1.go
+++ dup1.go
@@ -1,5 +1,8 @@
 // Dup1 will show the content of each line that appears more than once
 //when you type input at the terminal and shows a counter.
+// run : go run dup1.go  + ENTER + write your text and when you are done
+// press CTRL + Z (tried on Windows with git bash terminal)
+
 package main
 
 import (
@@ -9,15 +12,23 @@ import (
 )
 
 func main() {
+	// A map holds key/value pair - keys and values can be of any type.
+	// Here keys are of type String and values of type int.
+	//The make() method, creates a new empty map.
 	counter := make(map[string]int)
+	// the program read the argument(s) at command line,
+	// the line is the key into a map and the corresponding
+	// value is incremented
 	input := bufio.NewScanner(os.Stdin)
-	for input.Scan(){
+	for input.Scan() {
 		counter[input.Text()]++
 	}
 	// handeling eventual errors from input.Err()
-input.Err()
+	input.Err()
+	// contrary to other languages, we don't put
+	// the arguments of the for loop in parentheses but braces are required for the body.
 	for line, n := range counter {
-		if n>1 {
+		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
