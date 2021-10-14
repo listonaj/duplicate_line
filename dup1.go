a48b556e6b3f4d95a05062d81e52a5a54a037177
listonaj <jm.romain@outlook.com>
2021-10-14 12:14:43 -0700

    modfied the mod1 file

--- dup1.go
+++ dup1.go
@@ -1,3 +1,24 @@
 // Dup1 will show the content of each line that appears more than once
 //when you type input at the terminal and shows a counter.
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counter := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan(){
+		counter[input.Text()]++
+	}
+	// handeling eventual errors from input.Err()
+input.Err()
+	for line, n := range counter {
+		if n>1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
